a912335b44d466a3da4af293cf35dec7969644d4
My Name <my@hidden.email>
2020-12-12 20:19:30 +0300

    -

--- code_forces/CogTech/F2.py
+++ code_forces/CogTech/F2.py
@@ -1,3 +1,28 @@
+# dt = []
+#
+#
+# def prep_dt(n):
+#     global dt
+#     dt = [0] * n
+#     dt[0] = 1
+#     dt[1] = 2
+#     for k in range(2, n):
+#         if k % 2 == 1:
+#             dt[k] = dt[k - 1] * 2
+#         else:
+#             dt[k] = dt[k - 1] * 2 + 1
+#     # print('dt: ', dt)
+#
+#
+# def get_dt(a, n):
+#     sm = 0
+#     for k in range(n, -1, -1):
+#         if a[k] != 0:
+#             sm += 1
+#     d = dt[n - 2] if n > 1 else dt[n]
+#     return d - sm
+
+
 def activate(a, k):
     if a[k] == '1':
         return 0
@@ -13,6 +38,9 @@ def activate(a, k):
     return count
 
 
+dic = {}
+
+
 def deactivate(a, k):
     if a[k] == '0':
         return 0
@@ -20,17 +48,31 @@ def deactivate(a, k):
         a[k] = '0'
         return 1
 
-    count = activate(a, k - 1)
 
-    for n in range(k - 2, -1, -1):
-        count += deactivate(a, n)
+
+    count = activate(a, k - 1)
+    # print('bef:', a, k)
+    sm = ''.join(a[:k - 2])
+    # print('sm:', a, k, sm)
+    if sm in dic:
+        for p in range(k, -1, -1):
+            a[p] = '0'
+        count += dic[sm]
+    else:
+        count2 = 0
+        for n in range(k - 2, -1, -1):
+            count2 += deactivate(a, n)
+        dic[sm] = count2
+        count += count2
 
     a[k] = '0'
     count += 1
+
     return count
 
 
 def solve(a, b):
+    # prep_dt(len(a) * 2)
     count = 0
     for k in range(len(a) - 1, -1, -1):
         if a[k] != b[k]:
@@ -43,14 +85,14 @@ def solve(a, b):
 
 
 def main():
-    n = int(input())
-    a = [char for char in input()]
-    b = input()
+    n = int(input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
     print(solve(a, b))
 
 
 def test():
-    for n in range(1, 4):
+    for n in range(1, 17):
         a = ['1'] * n
         b = ['0'] * n
         print('----- ', n)
