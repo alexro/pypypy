2320c64ebad02394ac9154b6792df4dfd019b841
My Name <my@hidden.email>
2020-12-13 17:45:58 +0300

    -

--- code_forces/Inno/B2.py
+++ code_forces/Inno/B2.py
@@ -25,9 +25,10 @@ def solve(a):
 
 def read(s):
     s = s.split()
-    a = [0] * len(s)
+    a = []
     for k in range(len(s)):
-        a[k] = (int(s[k]), k)
+        if k == 0 or s[k-1] != s[k]:
+            a.append((int(s[k]), len(a), k))
     return a
 
 
@@ -36,7 +37,7 @@ def result(dic):
         print('No')
         return
 
-    res = ['Yes']
+    res = []
     for i, v in enumerate(dic):
         if len(dic[v]) != 2:
             print('No')
@@ -44,7 +45,9 @@ def result(dic):
         else:
             ia = dic[v][0]
             ib = dic[v][1]
-            res.append(str(ia[0]) + ' ' + str(ib[0]))
+            res.append(str(ia[2] + 1) + ' ' + str(ib[2] + 1))
+    print('Yes')
+    print(len(dic))
     for s in res:
         print(s)
 
@@ -59,10 +62,13 @@ def main():
 
 def test():
     a = read('2 5 5 2 10 9')
+    result(solve(a))
     a = read('2 1 1 2 2 9 8')
+    result(solve(a))
     a = read('2 3 4 5 1')
+    result(solve(a))
     a = read('3 2 1')
     result(solve(a))
 
 
-test()
+main()
